e4d97c0392c1aa3914608517f1405d178d347e6d
aibhleog <astro.hutchison@gmail.com>
2024-08-20 11:35:29 -0400

    first push for website :fire:

---
+++ includes/footer.js
@@ -0,0 +1,8 @@
+document.write("<ul class='icons'>");
+document.write("<li><a href='https://github.com/nasa-peer' class='icon brands alt fa-github'><span class='label'>GitHub</span></a></li>");
+document.write("</ul>");
+document.write("<ul class='copyright'>");
+document.write("<li><strong><font color='#589FCE'>EMAIL AT gmail.com</font></strong></li>");
+document.write("<li>&copy; NASA-PEER</li>");
+document.wirte("<li>Design: <a href='https://html5up.net'>HTML5 UP</a></li>");
+//document.write("</ul>");
---
+++ includes/navigate.js
@@ -0,0 +1,11 @@
+
+//document.write("<h1 class='strongFancy'><strong><a href='index.html'>NASA-PEER</a></strong></h1>");
+
+document.write("<ul class='links'>");
+document.write("<li><a href='about.html'>About NASA-PEER</a></li>");
+document.write("<li><a href='glance.html'>NASA-PEER at a glance</a></li>");
+document.write("<li><a href='programmatics.html'>Programmatics</a></li>");
+document.write("<li><a href='leadership.html'>Leadership</a></li>");
+document.write("<li><a href='cohort.html'>Current Cohort</a></li>");
+document.write("<li><a href='resources.html'>Resources</a></li>");
+document.write("</ul>");
